47cba78c6381856f38099ea06b309ff14dc7e6db
Adam <escaton18@gmail.com>
2016-07-25 10:31:10 +0200

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,10 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    //webserver = require('gulp-webserver'),
     jshint = require('gulp-jshint'),
     browserSync = require('browser-sync').create(),
     browserify = require('gulp-browserify'),
-    uglify = require('gulp-uglify'),
-    uniq = require('uniq');
+    uglify = require('gulp-uglify');
 
-
-//gulp.task('webserver', function () {
-//    gulp.src('app')
-//        .pipe(webserver({
-//            open: true
-//        }));
-//
-//});
 
 gulp.task('sass', function(){
     return gulp.src('app/styles/*.scss')
@@ -27,7 +17,7 @@ gulp.task('js', function () {
     return gulp.src(['./app/*/*.js', './app/*.js'])
         .pipe(browserify())
         .pipe(uglify())
-        .pipe(gulp.dest('./app/*.js'));
+        .pipe(gulp.dest('./build'));
 });
 
 gulp.task('js-watch', ['js'], browserSync.reload);
@@ -47,5 +37,6 @@ gulp.task('browser-sync', function() {
 
     gulp.watch('app/styles/*.scss', ['sass']);
     gulp.watch("app/*.html").on('change', browserSync.reload);
+    gulp.watch(['./app/*/*.js', './app/*.js'], ['js-watch']);
 });
 
